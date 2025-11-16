f4e4e0516fefe9f6ecafcc8dd1e2d0396737b939
Fauzi Saputra <karaktermobile1@gmail.com>
2025-11-17 02:00:50 +0700

    progress feature apps

--- app/src/main/java/com/example/sehatvibes/MainActivity.kt
+++ app/src/main/java/com/example/sehatvibes/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        loadFragment(HomeFragment())
+
+        val navbar = findViewById<BottomNavigationView>(R.id.navbar)
+        navbar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navHome -> loadFragment(HomeFragment())
+                R.id.navExplore -> loadFragment(ExploreFragment())
+                R.id.navStatistics -> loadFragment(StatisticsFragment())
+                R.id.navProfile -> loadFragment(ProfileFragment())
+                else -> false
+            }
         }
+    }
+
+    private fun loadFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+        return true
     }
 }
